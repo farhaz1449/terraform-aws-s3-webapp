825355c04085c4e59cc62f4f727d3dbf6ac62bac
Abu Farhaz <farhaz1449@gmail.com>
2024-04-17 03:27:43 -0400

    Fixed

--- main.tf
+++ main.tf
@@ -3,16 +3,15 @@
 
 resource "aws_s3_bucket" "bucket" {
   bucket_prefix = "${var.prefix}-${var.name}"
-
   force_destroy = true
 }
 
 resource "aws_s3_bucket_public_access_block" "bucket" {
   bucket = aws_s3_bucket.bucket.id
 
-  block_public_acls       = false
+  block_public_acls       = true
   block_public_policy     = false
-  ignore_public_acls      = false
+  ignore_public_acls      = true
   restrict_public_buckets = false
 }
 
@@ -33,16 +32,6 @@ resource "aws_s3_bucket_website_configuration" "bucket" {
   error_document {
     key = "error.html"
   }
-}
-
-resource "aws_s3_bucket_acl" "bucket" {
-  depends_on = [
-    aws_s3_bucket_public_access_block.bucket,
-    aws_s3_bucket_ownership_controls.bucket,
-  ]
-  bucket = aws_s3_bucket.bucket.id
-
-  acl = "public-read"
 }
 
 resource "aws_s3_bucket_policy" "policy" {
@@ -68,9 +57,9 @@ EOF
 }
 
 resource "aws_s3_object" "webapp" {
-  acl          = "public-read"
-  key          = "index.html"
   bucket       = aws_s3_bucket.bucket.id
-  content      = file("${path.module}/assets/index.html")
+  key          = "index.html"
+  source       = "${path.module}/assets/index.html"
   content_type = "text/html"
+  # Removed 'acl' attribute because the bucket is configured to block public ACLs
 }
